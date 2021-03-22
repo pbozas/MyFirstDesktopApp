12c62e372c1464b82dd9829d2802428573db1f3b
pbozas <fbozas@hotmail.com>
2021-03-22 15:02:12 +0200

    Add project files.

---
+++ MyFirstDesktopApp/Calculation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstDesktopApp
+{
+    public class Calculation
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+
+        public int Result { get => Number1 + Number2; }
+    }
+}
---
+++ MyFirstDesktopApp/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstDesktopApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calculation calculation = new Calculation();
+            calculation.Number1 = Convert.ToInt32(textBox1.Text);
+            calculation.Number2 = Convert.ToInt32(textBox2.Text);
+            textBox3.Text = calculation.Result.ToString();
+
+        }
+    }
+}
